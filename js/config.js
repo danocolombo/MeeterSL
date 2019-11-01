1ebe51ba65da3469ad12192c9d901bf9b512d3a2
dano@dcolombo.com <GR0mans1212!>
2019-11-01 00:22:00 -0400

    initial code from Nikhil Rao

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_0PVqSgpSd', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '5vlbq52ffjqsvmi8u1uk9oq29n' 
+    },
+};
